dc09e40116903d451c8f14764e2d15ccca529e6a
daniilamark <daniilamark@gmail.com>
2022-07-21 23:55:15 +0500

    add movie list widget

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -12,24 +12,68 @@ class MovieListWidget extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black.withOpacity(0.2)),
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 8,
-                    offset: const Offset(0, 2),
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black.withOpacity(0.2)),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 8,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: Row(
-                children: const [
-                  Image(image: AssetImage(AppImages.rickandmorty)),
-                ],
-              ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Row(
+                    children: [
+                      const Image(image: AssetImage(AppImages.rickandmorty)),
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            SizedBox(height: 20),
+                            Text(
+                              'Rick and Morty',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              'April 7, 2001',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              'This is package is an independent library that is not linked to your project. So theres no need to add it to your flutter project as it works as a global command line tool for all of your projects',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                    ],
+                  ),
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    // ignore: avoid_print
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("ssssssss");
+                    },
+                  ),
+                ),
+              ],
             ),
           );
         });
